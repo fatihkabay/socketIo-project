443aa2ce785cdb8709a8ab350ef6d13c4e1da6ad
Fatih Kabay <mfthkabay@gmail.com>
2023-10-12 22:08:34 +0300

    send message button click and writed hello in console

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
-const { Server  } = require('socket.io');
-const cors = require('cors');
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
 
 app.use(cors());
 
 const server = http.createServer(app);
 
-const io = new Server( server, {
-    cors: {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST"]
-    }
-})
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
 
 io.on("connection", (socket) => {
-    console.log(`User Connected: ${socket.id}`)
-})
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("send_message", (data) => {
+    socket.broadcast.emit("receive_message", data)
+  });
+});
 
 server.listen(3001, () => {
-    console.log("SERVER IS RUNNING")
-})
+  console.log("SERVER IS RUNNING");
+});
