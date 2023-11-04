ec69cb87a1b0b7f61b4b48287da820c9d157409e
Fatih Kabay <mfthkabay@gmail.com>
2023-11-04 23:51:35 +0300

    send message and disconnect function changed

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const app = express();
 const http = require("http");
-const { Server } = require("socket.io");
 const cors = require("cors");
-
+const { Server } = require("socket.io");
 app.use(cors());
 
 const server = http.createServer(app);
@@ -20,23 +19,18 @@ io.on("connection", (socket) => {
 
   socket.on("join_room", (data) => {
     socket.join(data);
-
-    socket.on("send_message", (data) => {
-      console.log(data);
-    });
-
-    socket.on("disconnect", () => {
-      console.log(`User Disconnected: ${socket.id}`);
-    });
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
   });
 
   socket.on("send_message", (data) => {
     socket.to(data.room).emit("receive_message", data);
   });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
 });
 
-io.on();
-
 server.listen(3001, () => {
-  console.log("SERVER IS RUNNING", "http://localhost:3001");
+  console.log("SERVER RUNNING");
 });
